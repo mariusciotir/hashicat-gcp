473a2eb9b10f30a60ec51c04648c8eee52bcd057
Marius Ciotir <marius.ciotir@gmail.com>
2021-05-20 16:40:08 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,7 @@
 module "network" {
   source  = "app.terraform.io/mc1-training/network/google"
   version = "2.5.0"
+  project_id = var.project_id
   subnets = [
     {
       subnet_name   = "mariusc-subnet"
